bdf7f19e41a72d92c169bb815b138f161207e0f8
Yoji <yoji-tanaka@cookpad.com>
2014-09-10 20:24:53 +0900

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -31,17 +31,28 @@ subject { page }
 	    expect(page).to have_selector("li##{item.id}", text:item.content)
 	  end
 	end
+
+    describe "follower/following counts" do
+      let(:other_user){FactoryGirl.create(:user)}
+      before do
+	    other_user.follow!(user)
+	    visit root_path
+	  end
+
+	  it { should have_link("0 following", href: following_user_path(user))}
+	  it { should have_link("1 followers", href: followers_user_path(user))}
+    end
+   end
   end
- end
 
- describe "Help page" do
-  before {visit root_path}
-  let(:heading) { 'Help' }
-  let(:page_title){''}
+  describe "Help page" do
+    before {visit root_path}
+    let(:heading) { 'Help' }
+    let(:page_title){''}
 
-  it_should_behave_like "all static pages"
-  it { should_not have_title('| Help')}
- end
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Help')}
+  end
 
  describe "About page" do
   before {visit root_path}
